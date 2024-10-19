c8dec05f468a7282e3047f3baeaf075db46ee513
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-19 20:00:17 +0530

    feat: add fo rjavascript

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -17,6 +17,8 @@
 
 	<link rel="stylesheet" href="{{ asset('admin-assets/plugins/dropzone/min/dropzone.min.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('admin-assets/plugins/summernote/summernote-bs4.min.css') }}">
+
     <link rel="stylesheet" href="{{ asset('admin-assets/css/custom.css') }}">
     <meta name = "csrf-token" content = "{{ csrf_token() }}">
 </head>
@@ -94,6 +96,9 @@
     <script src="{{ asset('admin-assets/js/adminlte.min.js') }}"></script>
 
 	<script src="{{ asset('admin-assets/plugins/dropzone/min/dropzone.min.js') }}"></script>
+
+    <script src="{{ asset('admin-assets/plugins/summernote/summernote.min.js') }}"></script>
+
     <!-- AdminLTE for demo purposes -->
     <script src="{{ asset('admin-assets/js/demo.js') }}"></script>
 
@@ -102,7 +107,14 @@
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
-        })
+        });
+
+        $(document).ready(function() {
+
+            $('.summernote').summernote({
+                height: 300
+            }) 
+        });
     </script>
     @yield('customJs')
 </body>
