247de1f57fa37fac5bc1278b84b0b65bab46d040
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-19 17:36:40 +0530

    feat: create blade files for brands CRUD

---
+++ resources/views/admin/brands/edit.blade.php
@@ -0,0 +1,143 @@
+{{-- DAshboard CHILD LAYOUT, parent layout - app.blade.php --}}
+
+@extends('admin.layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid my-2">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Edit Brand</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ route('brands.index') }}" class="btn btn-primary">Back</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="container-fluid">
+            <form action="{{ route('brands.store') }}" method="POST" id="createBrandForm" name="createBrandForm">
+                @csrf
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" id="name" class="form-control"
+                                        placeholder="Name" value="{{ $brand->name }}">
+                                    <p></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="slug">Slug</label>
+                                    <input type="text" name="slug" id="slug" class="form-control"
+                                        placeholder="Slug" value="{{ $brand->slug }}">
+                                    <p></p>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="status">Status</label>
+                                    <select type="text" name="status" id="status" class="form-control">
+                                        <option {{ $brand->status == 1 ? 'selected': '' }} value="1">Active</option>
+                                        <option {{ $brand->status == 0 ? 'selected': '' }} value="0">Block</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="pb-5 pt-3">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                    <a href="{{ route('brands.index') }}" class="btn btn-outline-dark ml-3">Cancel</a>
+                </div>
+            </form>
+        </div>
+        <!-- /.card -->
+    </section>
+    <!-- /.content -->
+@endsection
+
+@section('customJs')
+    <script>
+        $("#createBrandForm").submit(function(event) {
+            event.preventDefault();
+            var element = $(this);
+            $("button[type='submit']").prop('disabled', true);
+            $.ajax({
+                url: '{{ route("brands.update", $brand->id) }}',
+                type: 'PUT',
+                data: element.serializeArray(),
+                dataType: 'json',
+
+                success: function(response) {
+                    $("button[type='submit']").prop('disabled', false);
+                    if (response["status"] == true) {
+
+                        window.location.href = "{{ route('brands.index') }}";
+
+                        $('#name').removeClass('is-invalid').siblings('p').removeClass(
+                            'invalid-feedback').html("");
+
+                        $('#slug').removeClass('is-invalid').siblings('p').removeClass(
+                            'invalid-feedback').html("");
+
+
+                    } else {
+                        var errors = response['errors'];
+                        if (errors['name']) {
+                            $('#name').addClass('is-invalid').siblings('p').addClass('invalid-feedback')
+                                .html(errors['name']);
+                        } else {
+                            $('#name').removeClass('is-invalid').siblings('p').removeClass(
+                                'invalid-feedback').html("");
+                        }
+
+                        if (errors['slug']) {
+                            $('#slug').addClass('is-invalid').siblings('p').addClass('invalid-feedback')
+                                .html(errors['slug']);
+                        } else {
+                            $('#slug').removeClass('is-invalid').siblings('p').removeClass(
+                                'invalid-feedback').html("");
+                        }
+                    }
+                },
+
+                error: function(jqHXR, exception) {
+                    console.log('something went wrong');
+                }
+            })
+        });
+
+        $("#name").change(function() {
+            element = $(this);
+            $("button[type='submit']").prop('disabled', true);
+            $.ajax({
+                url: '{{ route('getSlug') }}',
+                type: 'get',
+                data: {
+                    name: element.val()
+                },
+                dataType: 'json',
+
+                success: function(response) {
+                    $("button[type='submit']").prop('disabled', false);
+                    if (response["status"] == true) {
+                        $("#slug").val(response["slug"]);
+                    }
+                }
+
+            });
+        });
+
+
+    </script>
+@endsection
