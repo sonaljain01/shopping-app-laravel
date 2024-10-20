46e072424a2f1ca9d0b6f487b03abba0c869337b
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-20 14:53:28 +0530

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,8 @@ Route::group(['prefix'=>'admin'], function () {
         Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
         Route::post('/products', [ProductController::class, 'store'])->name('products.store');
         Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+        Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+        Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
 
 
         Route::get('/getSlug', function(Request $request){
