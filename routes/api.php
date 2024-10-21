8dd4eb9133c048cd593a29c92b8b13fdd2dd6732
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-21 20:07:55 +0530

    update unwanted imports

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,10 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', [RegisterController::class, 'register']);
    
 });
 
-Route::get('test', function () {
-    return response()->json(['message' => 'API routes are working']);
-});
