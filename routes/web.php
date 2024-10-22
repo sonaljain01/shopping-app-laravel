01356e4ac4dfc12dbbe5b91f19170c90dd6018de
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-22 19:48:19 +0530

    fix: update the routes

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::group(['prefix'=>'admin'], function () {
         Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
         Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
         Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-        Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::post('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
         Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.delete');
 
         Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
@@ -54,7 +54,7 @@ Route::group(['prefix'=>'admin'], function () {
         Route::post('/products', [ProductController::class, 'store'])->name('products.store');
         Route::get('/products', [ProductController::class, 'index'])->name('products.index');
         Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-        Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+        Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update');
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.delete');
         Route::post('product/image/upload', [ProductImageController::class, 'upload'])->name('product.image.upload');
 
