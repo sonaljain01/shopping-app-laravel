d6290e2852994142058017a86bb8c7381416b3a5
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-22 19:51:12 +0530

    fix: update blades file

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -11,6 +11,6 @@
 
 <div class="alert alert-success alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h4><i class="icon fa fa-check"></i> 'Success!</h4>   {{ Session::has('success') }}
+    <h4><i class="icon fa fa-check"></i> 'Success!'</h4>{{ Session::get('success') }}
 </div>
 @endif
