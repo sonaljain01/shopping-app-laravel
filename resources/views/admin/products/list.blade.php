87071b6ad979624a61825b1a42f799a89f7560cc
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-22 19:51:43 +0530

    fix: update blades file

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Content Header (Page header) -->
-    <section class="content-header">					
+    <section class="content-header">
         <div class="container-fluid my-2">
             <div class="row mb-2">
                 <div class="col-sm-6">
@@ -38,7 +38,7 @@
                                 </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </form>
 
@@ -59,67 +59,76 @@
                         <tbody>
                             @if ($products->isNotEmpty())
                                 @foreach ($products as $product)
+                                    <tr>
+                                        <td>{{ $product->id }}</td>
+                                        <td>
+                                            @if (count($product->product_images) > 0)
+                                                <img src="{{ asset($product->product_images[0]->image) }}"
+                                                    class="img-thumbnail" width="50" />
+                                            
+                                            @endif
 
-                                @php
-                                    $procuctImage = $product->product_images->first();
-                                @endphp
-                                <tr>
-                                    <td>{{ $product->id }}</td>
-                                    <td>
-                                        @if (!empty($procuctImage->image))
-                                        <img src="{{ asset('uploads/product/large/'. $productImage->image) }}" class="img-thumbnail" width="50" />
-                                        @else
-                                        {{-- <img src="{{ asset('admin-assets/img/prod-5') }}" class="img-thumbnail" width="50"/>  --}}
-                                        @endif
-                                    
-                                    </td>
-                                    <td><a href="#">{{ $product->title }}</a></td>
-                                    <td>${{ $product->price }}</td>
-                                    <td>{{ $product->qty }} left in Stock</td>
-                                    <td>{{ $product->sku }}</td>											
-                                    <td>
+                                        </td>
 
-                                        @if ($product->status == 1)
-                                        <svg class="text-success-500 h-6 w-6 text-success" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                        </svg>                                       
-                                        @else
-                                        <svg class="text-danger h-6 w-6" xmlns="http://www.w3.org/2000/svg"
+                                        <td><a href="#">{{ $product->title }}</a></td>
+                                        <td>${{ $product->price }}</td>
+                                        <td>{{ $product->qty }} left in Stock</td>
+                                        <td>{{ $product->sku }}</td>
+                                        <td>
+
+                                            @if ($product->status == 1)
+                                                <svg class="text-success-500 h-6 w-6 text-success"
+                                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                                </svg>
+                                            @else
+                                                <svg class="text-danger h-6 w-6" xmlns="http://www.w3.org/2000/svg"
                                                     fill="none" viewBox="0 0 24 24" stroke-width="2"
                                                     stroke="currentColor" aria-hidden="true">
                                                     <path stroke-linecap="round" stroke-linejoin="round"
                                                         d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z">
                                                     </path>
                                                 </svg>
-                                        @endif
-                                        
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('products.edit', $product->id) }}">
-                                            <svg class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-                                            </svg>
-                                        </a>
-                                        <a href="#"  onclick="deleteProduct({{ $product->id }})" class="text-danger w-4 h-4 mr-1">
-                                            <svg wire:loading.remove.delay="" wire:target="" class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                <path	ath fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                                              </svg>
-                                        </a>
-                                    </td>
-                                </tr>
-                                @endforeach                           
+                                            @endif
+
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('products.edit', $product->id) }}">
+                                                <svg class="filament-link-icon w-4 h-4 mr-1"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <path
+                                                        d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
+                                                    </path>
+                                                </svg>
+                                            </a>
+                                            <a href="#" onclick="deleteProduct({{ $product->id }})"
+                                                class="text-danger w-4 h-4 mr-1">
+                                                <svg wire:loading.remove.delay="" wire:target=""
+                                                    class="filament-link-icon w-4 h-4 mr-1"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <path ath fill-rule="evenodd"
+                                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                        clip-rule="evenodd"></path>
+                                                </svg>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             @else
-                                    
                                 <tr>
                                     <td>
                                         Recordes not found
                                     </td>
                                 </tr>
                             @endif
-                            
-                            
+
+
                         </tbody>
-                    </table>										
+                    </table>
                 </div>
                 <div class="card-footer clearfix">
                     {{ $products->links() }}
@@ -139,28 +148,28 @@
 @endsection
 
 @section('customJs')
-<script>
-    function deleteProduct(id) {
-        var url = '{{ route('products.delete', 'ID') }}';
-        var newUrl = url.replace("ID", id);
-        if (confirm("Are you sure you want to delete this product?")) {
-            $.ajax({
-                url: newUrl,
-                type: 'DELETE',
-                data: {},
-                dataType: 'json',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function(response) {
+    <script>
+        function deleteProduct(id) {
+            var url = '{{ route('products.delete', 'ID') }}';
+            var newUrl = url.replace("ID", id);
+            if (confirm("Are you sure you want to delete this product?")) {
+                $.ajax({
+                    url: newUrl,
+                    type: 'DELETE',
+                    data: {},
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(response) {
 
-                    if (response["status"]) {
+                        if (response["status"]) {
 
-                        window.location.href = "{{ route('products.index') }}"
+                            window.location.href = "{{ route('products.index') }}"
+                        }
                     }
-                }
-            });
+                });
+            }
         }
-    }
-</script>
+    </script>
 @endsection
