066ad074b3ceb825ffe68f2c8824c8a4429b2f41
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-22 19:51:52 +0530

    fix: update blades file

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -388,7 +388,7 @@
                         <div class="card product-card">
                             <div class="product-image position-relative">
                                 <a href="" class="product-img"><img class="card-img-top"
-                                        src="{{ asset('front-assets/images/product-1.jpg') }}" alt=""></a>
+                                    src="{{ asset('front-assets/images/product-1.jpg') }}" alt=""></a>
                                 <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
 
                                 <div class="product-action">
