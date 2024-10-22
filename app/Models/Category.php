083d33f1b2de844b4b29a4cc24289c2ed09d2b56
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-22 16:41:23 +0530

    feat: update category controller for child

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'slug', 
+        'status', 
+        'showHome', 
+        'is_subcategory', 
+        'parent_id'
+    ];
+
+    // Relation to parent category
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    // Relation to child categories
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
