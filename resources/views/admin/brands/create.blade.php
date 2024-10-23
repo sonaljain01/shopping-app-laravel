bb8caedf9eb1b4e222f1813d824d87f816e77203
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-23 15:17:51 +0530

    fix: update blade

--- resources/views/admin/brands/create.blade.php
+++ resources/views/admin/brands/create.blade.php
@@ -21,7 +21,8 @@
     <section class="content">
         <!-- Default box -->
         <div class="container-fluid">
-            <form action="" name="createBrandForm" id="createBrandForm" method="POST">
+            <form action="{{ route('brands.store') }}" name="createBrandForm" id="createBrandForm" method="POST" >
+                @csrf
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
@@ -30,18 +31,11 @@
                                     <label for="name">Name</label>
                                     <input type="text" name="name" id="name" class="form-control"
                                         placeholder="Name">
-                                    <p></p>
+                                 
                                 </div>
 
                             </div>
-                            <div class="col-md-6">
-                                <div class="mb-3">
-                                    <label for="email">Slug</label>
-                                    <input type="text" name="slug" id="slug" class="form-control"
-                                        placeholder="Slug">
-                                    <p></p>
-                                </div>
-                            </div>
+                            
                             <div class="col-md-6">
                                 <div class="mb-3">
                                     <label for="status">Status</label>
@@ -65,78 +59,3 @@
     <!-- /.content -->
 @endsection
 
-@section('customJs')
-    <script>
-        $("#createBrandForm").submit(function(event) {
-            event.preventDefault();
-            var element = $(this);
-            $("button[type=submit]").prop('disabled', true);
-            $.ajax({
-                url: '{{ route('brands.store') }}',
-                type: 'POST',
-                data: element.serializeArray(),
-                dataType: 'json',
-
-                success: function(response) {
-                    $("button[type='submit']").prop('disabled', false);
-                    window.location.href = "{{ route('brands.index') }}";
-                    if (response["status"] == true) {
-
-                        // window.location.href = "{{ route('categories.index') }}";
-
-                        // $('#name').removeClass('is-invalid').siblings('p').removeClass(
-                        //     'invalid-feedback').html("");
-
-                        // $('#slug').removeClass('is-invalid').siblings('p').removeClass(
-                        //     'invalid-feedback').html("");
-
-
-                    } else {
-                        var errors = response['errors'];
-                        if (errors.name) {
-                            $("#name").addClass('is-invalid')
-                                .siblings('p')
-                                .addClass('invalid-feedback').html(errors['name']);
-                        } else {
-                            $("#name").removeClass('is-invalid')
-                                .siblings('p')
-                                .removeClass('invalid-feedback').html("");
-                        }
-
-                        if (errors['slug']) {
-                            $('#slug').addClass('is-invalid').siblings('p').addClass('invalid-feedback')
-                                .html(errors['slug']);
-                        } else {
-                            $('#slug').removeClass('is-invalid').siblings('p').removeClass(
-                                'invalid-feedback').html("");
-                        }
-                    }
-                },
-
-                error: function(jqHXR, exception) {
-                    console.log('something went wrong');
-                }
-            })
-        });
-
-        $("#name").change(function() {
-            element = $(this);
-            $("button[type='submit']").prop('disabled', true);
-            $.ajax({
-                url: '{{ route('getSlug') }}',
-                type: 'get',
-                data: {
-                    title: element.val()
-                },
-                dataType: 'json',
-
-                success: function(response) {
-                    $("button[type='submit']").prop('disabled', false);
-                    if (response["status"] == true) {
-                        $("#slug").val(response["slug"]);
-                    }
-                }
-            });
-        });
-    </script>
-@endsection
