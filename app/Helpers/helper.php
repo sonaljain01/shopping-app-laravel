f355bfd0a5141f246f06698457783473620e8e8d
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-23 15:19:20 +0530

    fix: update helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -4,5 +4,5 @@ use App\Models\Category;
 function getCategories()
 {
     // Category::orderBy('name', 'ASC')->get();
-    return Category::with('children')->where('showHome', 'Yes')->orderBy('id', 'desc')->get();
+    return Category::with('children')->where('showHome', 'Yes')->orderBy('id', 'desc')->take(4)->get();
 }
