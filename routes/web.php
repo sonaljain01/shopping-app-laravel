0cf949d7d280002f892ac67bc0af53b89a7a0c0d
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-23 19:14:55 +0530

    feat: create routes

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::group(['prefix'=>'admin'], function () {
         Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
         Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update');
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.delete');
+        Route::get('/product/{slug}', [ProductController::class, 'show'])->name('product.show');
 
         Route::get('/getSlug', function(Request $request){
             $slug = '';
