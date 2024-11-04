6ddf9a4f7935b1c990b43e65aee0f24b96d31120
sonaljain01 <jainsonal8630407466@gmail.com>
2024-11-04 14:29:47 +0530

    feat: update the cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,45 +10,7 @@ class CartController extends Controller
 {
     public function addToCart(Request $request, $productId)
     {
-        // $product = Product::find($productId);
-        // // $user = Auth::user();
-        // if (!session()->has('cart')) {
-        //     session()->put('cart', []);
-        // }
-
-        // // $cartItem = Cart::where('user_id', $user->id)->where('product_id', $product->id)->first();
-
-        // // if($cartItem){
-        // //     $cartItem->increment('quantity');
-        // //     $cartItem->save();  
-        // // }else{
-        // //     Cart::create([
-        // //         'user_id' => $user->id,
-        // //         'product_id' => $product->id,
-        // //         'quantity' => 1
-        // //     ]);
-        // // }
-        // $cart = session()->get('cart');
-
-        // // Check if the product is already in the cart
-        // if (array_key_exists($productId, $cart)) {
-        //     // If it exists, increment the quantity
-        //     $cart[$productId]['quantity']++;
-        // } else {
-        //     // If it doesn't exist, add it to the cart
-        //     $cart[$productId] = [
-        //         'title' => $product->title,
-        //         'price' => $product->price,
-        //         'quantity' => 1,
-        //         'image' => $product->product_images->isNotEmpty() ? $product->product_images[0]->image : 'path/to/default/image.jpg',
-        //     ];
-        // }
-
-        // // Update the cart in the session
-        // session()->put('cart', $cart);
-        // return redirect()->back()->with('success', 'Product added to cart successfully');
-
-        // dd(session()->get('cart'));
+        
         $product = Product::find($productId);
         if (!$product) {
             return redirect()->back()->with('error', 'Product not found.');
