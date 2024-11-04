3376673c7d4089e15f5f02830f769d13c4817792
sonaljain01 <jainsonal8630407466@gmail.com>
2024-11-04 14:30:08 +0530

    feat: create a wishlist controller

--- app/Http/Controllers/Wishlistcontroller.php
+++ app/Http/Controllers/Wishlistcontroller.php
@@ -13,22 +13,51 @@ class Wishlistcontroller extends Controller
             'product_id' => 'required|exists:products,id',
         ]);
 
-        Wishlist::create([
-            'product_id' => $validatedData['product_id'],
-            'user_id' => auth()->check() ? auth()->user()->id : null,
-            'guest_id' => auth()->check() ? null : session('guest_id'),
-        ]);
+        $userId = auth()->check() ? auth()->user()->id : null;
+        $guestId = auth()->check() ? null : session('guest_id');
+
+        // Check for existing wishlist entry to avoid duplicates
+        $existingWishlistItem = Wishlist::where('product_id', $validatedData['product_id'])
+            ->where(function ($query) use ($userId, $guestId) {
+                $query->where('user_id', $userId)->orWhere('guest_id', $guestId);
+            })
+            ->first();
+
+        if (!$existingWishlistItem) {
+            Wishlist::create([
+                'product_id' => $validatedData['product_id'],
+                'user_id' => $userId,
+                'guest_id' => $guestId,
+            ]);
+        }
 
         return redirect()->back()->with('success', 'Product added to wishlist successfully!');
     }
 
     public function index()
     {
-        $wishlists = Wishlist::where('user_id', auth()->user()->id)
-            ->orWhere('guest_id', session('guest_id'))
+        $wishlists = Wishlist::where('guest_id', session('guest_id'))
+            // ->orWhere('guest_id', session('guest_id'))
             ->with('product')
             ->get();
 
         return view('front.wishlist', compact('wishlists'));
+    }
+
+    public function remove($id)
+    {
+        $wishlist = Wishlist::find($id);
+        $wishlist->delete();
+
+        return redirect()->back()->with('success', 'product removed form wishlist');
+    }
+
+    public function getWishlistCount()
+    {
+        $wishlistCount = Wishlist::where('user_id', auth()->id())
+            ->orWhere('guest_id', session('guest_id'))
+            ->count();
+
+        return $wishlistCount;
     }
 }
