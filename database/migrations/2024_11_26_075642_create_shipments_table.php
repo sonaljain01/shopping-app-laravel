611c824721b7d8645224970582bb90e8ab7e7b9b
sonaljain01 <jainsonal8630407466@gmail.com>
2024-11-27 21:09:57 +0530

    updations

--- database/migrations/2024_11_26_075642_create_shipments_table.php
+++ database/migrations/2024_11_26_075642_create_shipments_table.php
@@ -11,21 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shipments', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
-            $table->string('courier_name');
-            $table->foreignId('pickup_address_id')->constrained('pickup_addresses')->onDelete('cascade');
-            $table->decimal('actual_weight', 8, 2);
-            $table->decimal('volumetric_weight', 8, 2);
-            $table->decimal('chargeable_weight', 8, 2);
-            $table->string('platform');
-            $table->integer('shipment_id');        
-            $table->string('status');
-            // store the channel_order_id
-            $table->string('channel_order_id')->nullable();    
-            $table->timestamps();
-        });
+        // Schema::create('shipments', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+        //     $table->string('courier_name');
+        //     $table->foreignId('pickup_address_id')->constrained('pickup_addresses')->onDelete('cascade');
+        //     $table->decimal('actual_weight', 8, 2);
+        //     $table->decimal('volumetric_weight', 8, 2);
+        //     $table->decimal('chargeable_weight', 8, 2);
+        //     $table->string('platform');
+        //     $table->integer('shipment_id');        
+        //     $table->string('status');
+        //     // store the channel_order_id
+        //     $table->string('channel_order_id')->nullable();    
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('shipments');
+        // Schema::dropIfExists('shipments');
     }
-};
+}; 
