e940b86fcde7261ba3fde8c22ec41ab49ad8bbae
sonaljain01 <jainsonal8630407466@gmail.com>
2024-11-28 21:42:16 +0530

    updations

--- app/Http/Controllers/admin/ShipRocketController.php
+++ app/Http/Controllers/admin/ShipRocketController.php
@@ -25,103 +25,10 @@ class ShipRocketController extends Controller
     }
 
 
-    // public function createOrder($data, $pickupaddress)
-    // {
-    //     $this->pickup = pickupAddress::where('tag', $pickupaddress)->first()->id;
-    //     try {
-    //         $products = [];
-    //         $totalLength = 0;
-    //         $totalBreadth = 0;
-    //         $totalHeight = 0;
-    //         // $totalWeight = 0;
-    //         foreach ($data->products as $product) {
-    //             $products[] = [
-    //                 'name' => $product->product->title,
-    //                 'sku' => $product->product->sku,
-    //                 'units' => $product->quantity,
-    //                 'selling_price' => $product->price / $product->quantity,
-    //                 'discount' => '',
-    //                 'tax' => '',
-    //                 'hsn' => 441122,
-    //             ];
-
-    //             $totalLength += (int) $product->product->length;
-    //             $totalBreadth += (int) $product->product->breath;
-    //             $totalHeight += (int) $product->product->height;
-    //             // $totalWeight += $product->product->weight;
-    //         }
-    //         // Make the API request to ShipRocket
-    //         $api = Http::withHeaders([
-    //             'Content-Type' => 'application/json',
-    //             'Authorization' => 'Bearer ' . $this->token,
-    //         ])->post('https://apiv2.shiprocket.in/v1/external/orders/create/adhoc', [
-    //                     'order_id' => $data->id,
-    //                     'order_date' => $data->created_at->format('Y-m-d H:i:s'),
-    //                     'pickup_location' => $pickupaddress ?? 'home',
-    //                     'channel_id' => $this->channelId,
-    //                     'comment' => '',
-    //                     'billing_customer_name' => $data->user->name,
-    //                     'billing_last_name' => '',
-    //                     'billing_address' => $data->address->address,
-    //                     'billing_address_2' => '',
-    //                     'billing_city' => $data->address->city,
-    //                     'billing_pincode' => $data->address->pincode,
-    //                     'billing_state' => $data->address->state,
-    //                     'billing_country' => $data->address->country,
-    //                     'billing_email' => $data->user->email,
-    //                     'billing_phone' => $data->user->phone_number,
-    //                     'shipping_is_billing' => true,
-    //                     'shipping_customer_name' => '',
-    //                     'shipping_last_name' => '',
-    //                     'shipping_address' => '',
-    //                     'shipping_address_2' => '',
-    //                     'shipping_city' => '',
-    //                     'shipping_pincode' => '',
-    //                     'shipping_country' => '',
-    //                     'shipping_state' => '',
-    //                     'shipping_email' => '',
-    //                     'shipping_phone' => '',
-    //                     'order_items' => $products,
-    //                     'payment_method' => $data->payment_method == 'cod' ? 'postpaid' : 'prepaid',
-    //                     'shipping_charges' => 0,
-    //                     'giftwrap_charges' => 0,
-    //                     'transaction_charges' => 0,
-    //                     'total_discount' => 0,
-    //                     'sub_total' => $data->total,
-    //                     'length' => $totalLength,
-    //                     'breadth' => $totalBreadth,
-    //                     'height' => $totalHeight,
-    //                     'weight' => 2.5,
-    //                 ]);
-
-    //         // Check if the request was successful
-    //         if ($api->successful()) {
-    //             return $api; // Return the successful response as an array
-    //         } else {
-    //             // Log the error response for debugging
-    //             Log::error('ShipRocket API Error: ' . $api->body());
-    //             return [
-    //                 'success' => false,
-    //                 'message' => 'Failed to create order in ShipRocket. Error: ' . $api->body(),
-    //             ];
-    //         }
-    //     } catch (\Exception $e) {
-    //         // // Log the exception message
-    //         // Log::error('Exception while creating ShipRocket order: ' . $e->getMessage());
-    //         // return [
-    //         //     'success' => false,
-    //         //     'message' => 'An exception occurred while processing the order: ' . $e->getMessage(),
-    //         // ];
-    //         return $e->getMessage();
-    //     }
-    // }
-
     public function createOrder($data, $pickupAddressTag)
     {
         try {
-            // echo collect($data);
-            // dd();
-            // Fetch the pickup address using the tag
+            
             $pickupAddress = PickupAddress::where('tag', $pickupAddressTag)->first();
 
             if (!$pickupAddress) {
@@ -207,14 +114,9 @@ class ShipRocketController extends Controller
 
     public function storeShipment($data)
     {
-        if (!isset($data['order_id']) || !isset($data['channel_order_id']) || !isset($data['shipment_id']) || !isset($data['status'])) {
-            return [
-                'status' => false,
-                'message' => 'Required keys are missing in the data array',
-            ];
-        }
+        
         $shiprocket = Shipment::updateOrCreate([
-            'order_id' => $data['order_id'],
+            'order_id' => $data['order_id'] ?? null,
             'channel_order_id' => $data['channel_order_id'],
             'shipment_id' => $data['shipment_id'],
             'courier_name' => "",
