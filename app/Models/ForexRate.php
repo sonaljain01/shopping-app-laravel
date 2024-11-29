f1f4f37b122bf0b0e580b5f98d2fd4bf75a1e31c
sonaljain01 <jainsonal8630407466@gmail.com>
2024-11-29 17:34:12 +0530

    feat: create the model for same

---
+++ app/Models/ForexRate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class ForexRate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['base_currency', 'target_currency', 'rate'];
+}
